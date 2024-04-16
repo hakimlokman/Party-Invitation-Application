e4e3ef7194dac365c91a1069efe44ac646747ef5
Md Lokman Hakim <lokman.idb56@gmail.com>
2024-04-17 01:00:39 +0600

    first commit

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Rsvp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Rsvp(GuestRseponse rseponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Add(rseponse);
+                return View("Thanks", rseponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [ChildActionOnly]
+        public ActionResult Attendees()
+        {
+            return View(Repository.Rseponses.Where(x => x.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        private static Dictionary<string, GuestRseponse> response;
+
+        static Repository()
+        {
+            response = new Dictionary<string, GuestRseponse>();
+            response.Add("Shovon", new GuestRseponse { Name = "Shovon", Email = "shovon@gmail.com", WillAttend = true });
+            response.Add("Johurul", new GuestRseponse { Name = "Johurul", Email = "johurul@gmail.com", WillAttend = true });
+            response.Add("Rakib", new GuestRseponse { Name = "Rakib", Email = "rakib@gmail.com", WillAttend = true });
+
+        }
+        public static void Add(GuestRseponse newResponse)
+        {
+            string key = newResponse.Name.ToLowerInvariant();
+            if(response.ContainsKey(key))
+            {
+                response[key] = newResponse;
+            }
+            else
+            {
+                response.Add(key, newResponse);
+            }
+        }
+        public static IEnumerable<GuestRseponse> Rseponses {
+            get { return response.Values;}
+                }
+    }
+}
